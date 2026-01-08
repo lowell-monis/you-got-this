73f0bfc0dea4049c7bd7e7dec2219c8a3085990d
Lowell Monis <144663190+lowell-monis@users.noreply.github.com>
2026-01-08 00:55:27 -0500

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -6,7 +6,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
 import Dashboard from "@/pages/Dashboard";
 
-function Router() {
+function Routes() {
   return (
     <Switch>
       <Route path="/" component={Dashboard} />
@@ -19,8 +19,10 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
+        <Router base="/you-got-this">
+          <Routes />
+        </Router>
         <Toaster />
-        <Router />
       </TooltipProvider>
     </QueryClientProvider>
   );
